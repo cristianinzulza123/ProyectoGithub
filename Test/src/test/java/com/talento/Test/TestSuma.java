5216649865825427a1efdcaed0ae6915de2f821c
crist <crist@LAPTOP-LRMTTMT7>
2020-10-05 20:04:16 -0300

    Primera subida del proyecto

---
+++ Test/src/test/java/com/talento/Test/TestSuma.java
@@ -0,0 +1,20 @@
+package com.talento.Test;
+
+import junit.framework.TestCase;
+
+public class TestSuma extends TestCase{
+
+	public void testSumar() {
+		Suma suma = new Suma();
+		double resultado = suma.sumar(1.0, 1.0);
+		//Método que compara el valor obtenido por el método sumar y el valor real.
+		assertEquals(2.0, resultado);
+	}
+	
+	public void testDuplicar() {
+		Suma suma = new Suma();
+		double resultado = suma.duplicar(1.0);
+		assertEquals(2.0, resultado);
+	}
+	
+}
